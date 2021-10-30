8ac0861bf8e8d99c1f1aa0006285e935b3d760d8
Jung Jun Hyeok <ted0505@naver.com>
2021-10-31 07:02:08 +0900

    add color var and pos_2, vel_2 vector

--- ProjectFile/Ball.js
+++ ProjectFile/Ball.js
@@ -4,7 +4,12 @@ class Ball {
     this.vel = createVector(random(-5, 5), random(-5, 5));
     this.acc = createVector(0,0.1);
 
+    this.colors = 5;
+
     this.w = 11;
+
+    this.pos_2 = createVector(0, 0);
+    this.vel_2 = createVector(0, 0);
   }
 
    chkEdge() {
@@ -29,8 +34,22 @@ class Ball {
   chk() {
     let d = dist(this.pos.x, gray_ball.pos.x, this.pos.y, gray_ball.pos.y);
     if( d >= 175 ) {
-      fill(255);
-      rect(this.pos.x, this.pos.y, 15, 15);
+
+      fill(random(200,255));
+
+      // this.vel_2.x += sin(random(1,2)) * 0.1;
+      // this.vel_2.y += cos(1.2) * 0.1;
+      //
+      // this.pos_2.add(this.vel_2);
+
+      this.pos_2.x = this.pos.x;
+      this.pos_2.y = this.pos.y;
+
+      rect(this.pos_2.x, this.pos_2.y, 15, 15);
+
+      for(let i=0;i<255;i++) {
+        this.colors = random(255);
+      }
       this.vel.mult(-1);
       // soundFile.setVolume(0.5);
       // soundFile.play();
@@ -47,7 +66,8 @@ class Ball {
   }
 
   display() {
-    fill(5);
+    // if(this.colors >= 255) this.colors = 5;
+    fill(this.colors);
 	noStroke();
     ellipse(this.pos.x, this.pos.y, this.w, this.w);
   }
